685545ea13fbe445b50410647bd91fe634719566
huby2358 <hby2358@163.com>
2024-06-20 16:40:29 +0800

    multi cn setting

--- pkg/cnservice/cnclient/client.go
+++ pkg/cnservice/cnclient/client.go
@@ -15,7 +15,6 @@
 package cnclient
 
 import (
-	"fmt"
 	"sync"
 	"time"
 
@@ -78,9 +77,9 @@ func (c *CNClient) NewStream(backend string) (morpc.Stream, error) {
 		return nil, moerr.NewInternalErrorNoCtx("cn client is not ready")
 	}
 
-	if backend == c.localServiceAddress {
-		return nil, moerr.NewInternalErrorNoCtx(fmt.Sprintf("remote run pipeline in local: %s", backend))
-	}
+	// if backend == c.localServiceAddress {
+	// 	return nil, moerr.NewInternalErrorNoCtx(fmt.Sprintf("remote run pipeline in local: %s", backend))
+	// }
 	return c.client.NewStream(backend, true)
 }
 
