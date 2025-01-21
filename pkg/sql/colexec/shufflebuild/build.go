0b4a74cf1852e74e5a5808d22b0e9d7edcdd1521
huby2358 <hby2358@163.com>
2025-01-21 15:16:41 +0800

    fix

--- pkg/sql/colexec/shufflebuild/build.go
+++ pkg/sql/colexec/shufflebuild/build.go
@@ -120,7 +120,7 @@ func (ctr *container) collectBuildBatches(shuffleBuild *ShuffleBuild, proc *proc
 		analyzer.Alloc(int64(result.Batch.Size()))
 		fmt.Println("666666 analyzer.Alloc is   ", int64(result.Batch.Size()))
 		ctr.hashmapBuilder.InputBatchRowCount += result.Batch.RowCount()
-		fmt.Println("666666 InputBatchRowCount   ", ctr.hashmapBuilder.InputBatchRowCount, "ctr is ", ctr)
+		fmt.Printf("666666 InputBatchRowCount %d,  ctr is %p  \n", ctr.hashmapBuilder.InputBatchRowCount, ctr)
 		err = ctr.hashmapBuilder.Batches.CopyIntoBatches(result.Batch, proc)
 		if err != nil {
 			return err
