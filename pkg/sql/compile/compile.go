c7c94211a34cccfc7f0747414f03cefe604631ec
huby2358 <hby2358@163.com>
2025-02-07 10:45:37 +0800

    fix

--- pkg/sql/compile/compile.go
+++ pkg/sql/compile/compile.go
@@ -3753,7 +3753,7 @@ func (c *Compile) newShuffleJoinScopeList(probeScopes, buildScopes []*Scope, n *
 	}
 
 	// dop := max(int(n.Stats.Dop)/8, 1)
-	dop := 2
+	dop := 1
 	bucketNum := len(cnlist) * dop
 	shuffleProbes := make([]*Scope, 0, bucketNum)
 	shuffleBuilds := make([]*Scope, 0, bucketNum)
